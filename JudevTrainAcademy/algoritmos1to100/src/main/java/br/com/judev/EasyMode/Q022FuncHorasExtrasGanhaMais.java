77f7fc7f6c3112b70790f6ec16adb4370f48d1f3
Junior-Stranner <JuniorStranner@gmail.com>
2024-06-03 15:13:40 -0300

    I made several improvements and adding vectors

--- JudevTrainAcademy/algoritmos1to100/src/main/java/br/com/judev/EasyMode/Q022FuncHorasExtrasGanhaMais.java
+++ JudevTrainAcademy/algoritmos1to100/src/main/java/br/com/judev/EasyMode/Q022FuncHorasExtrasGanhaMais.java
@@ -3,25 +3,45 @@ package br.com.judev.EasyMode;
 import java.util.Scanner;
 
 public class Q022FuncHorasExtrasGanhaMais {
-     public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+        String[] nomeFunc = new String[2];
+        double[] salTotalFunc = new double[2];
 
-        System.out.println("Digite o número total de horas trabalhadas no mês:");
-        int horasTrabalhadas = Integer.parseInt(in.nextLine());
+        for (int x = 0; x < nomeFunc.length; x++) {
+            System.out.println("Digite o nome do Funcionário:");
+            nomeFunc[x] = in.nextLine();
 
-        System.out.println("Digite o salário por hora:");
-        double salarioPorHora = Double.parseDouble(in.nextLine());
+            System.out.println("Digite o número total de horas trabalhadas no mês:");
+            int horasTrabalhadas = Integer.parseInt(in.nextLine());
 
-         // Definição do limite de horas mensais (4 semanas de 40 horas cada)
-         int limiteHorasMensais = 160;
+            System.out.println("Digite o salário por hora:");
+            double salarioPorHora = Double.parseDouble(in.nextLine());
 
-        double salarioFunc = limiteHorasMensais * salarioPorHora;
-        System.out.println("Salário do funcionario que trabalhou "+limiteHorasMensais+ "no mês recebe "+salarioFunc);
+            int limiteHorasMensais = 160;
+            int horasExtras = 0;
+            salTotalFunc[x] = salarioPorHora * horasTrabalhadas;
 
-        double salarioTotal = salarioFunc * 1.5;
+            if (horasTrabalhadas > limiteHorasMensais) {
+                horasExtras = horasTrabalhadas - limiteHorasMensais;
+                double pagamentoHorasExtras = salarioPorHora * horasExtras * 1.5;
+                salTotalFunc[x] = (limiteHorasMensais * salarioPorHora) + pagamentoHorasExtras;
 
-        System.out.println("O salário total do funcionário é com as horas de trabalhadas de "+horasTrabalhadas+" no mês  " + salarioTotal);
+                System.out.println("-------------------------------------------"
+                    + "\nSalário do funcionário " + nomeFunc[x] + " que trabalhou " + horasTrabalhadas + " horas no mês"
+                    + "\nreceberá 50% a mais das horas extras que foram: " + horasExtras + " hora(s).");
+            } else {
+                System.out.println("Você trabalhou " + horasTrabalhadas + " horas. Não há horas extras."
+                    + "\nSalário normal de: " + salTotalFunc[x]);
+            }
 
-     }
+            System.out.println("O salário total do funcionário " + nomeFunc[x] + ", com as horas trabalhadas de " + horasTrabalhadas + " no mês, é de: " + salTotalFunc[x]);
+        }
+
+        for (int i = 0; i < nomeFunc.length; i++) {
+            System.out.println("-----------------------------------");
+            System.out.println("O salário total do funcionário " + nomeFunc[i] + " é de: " + salTotalFunc[i]);
+        }
+    }
 }
--- JudevTrainAcademy/algoritmos1to100/src/main/java/br/com/judev/EasyMode/Q25SladoCliente.java
+++ JudevTrainAcademy/algoritmos1to100/src/main/java/br/com/judev/EasyMode/Q25SladoCliente.java
@@ -7,8 +7,12 @@ public class Q25SladoCliente {
         
         Scanner in = new Scanner(System.in);
 
+        int numConta[] = new int[2];
+        double saldoAtual[] = new double[numConta.length];
+
+        for(int x = 0; x < numConta.length; x++){
        System.out.println("Numero da conta do cliente ");
-       int numConta = Integer.parseInt(in.nextLine());
+       numConta[x] = Integer.parseInt(in.nextLine());
 
        System.out.println("Saldo da conta do cliente ");
        double saldo = Double.parseDouble(in.nextLine());
@@ -19,12 +23,16 @@ public class Q25SladoCliente {
        System.out.println("credito da conta do cliente ");
        double credito = Double.parseDouble(in.nextLine());
 
-       double saldoAtual = saldo - (debito + credito);
+      saldoAtual[x] = saldo - (debito + credito);
 
-       if(saldoAtual < 0 ){
-        System.out.println("Saldo Atual Negativo !! "+saldoAtual);
+       if(saldoAtual[x] < 0 ){
+        System.out.println("Saldo Atual Negativo !! "+saldoAtual[x]);
        }else{
-       System.out.println("Saldo Atual Positivo !! "+saldoAtual);
+       System.out.println("Saldo Atual Positivo !! "+saldoAtual[x]);
        }
+     }
+     for(int x = 0; x < numConta.length; x++){
+        System.out.println("Conta número "+numConta[x]+ " com Saldo Atual de : "+saldoAtual[x]);
+     }
     }
 }
