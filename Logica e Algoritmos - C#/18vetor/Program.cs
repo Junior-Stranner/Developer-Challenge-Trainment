e56f4391f6f9e29b757b96759dbc033c22d9890d
Junior-Stranner <JuniorStranner@gmail.com>
2024-06-11 10:17:50 -0300

    new logic trainment in C#

---
+++ Logica e Algoritmos - C#/18vetor/Program.cs
@@ -0,0 +1,8 @@
+﻿//Vetor de nomes
+String[] nomes = {"Junior","Juliana","Ralf", "Marcela"};
+
+//Exibir os nomes 
+Console.WriteLine(nomes[0]);
+Console.WriteLine(nomes[1]);
+Console.WriteLine(nomes[2]);
+Console.WriteLine(nomes[3]);
