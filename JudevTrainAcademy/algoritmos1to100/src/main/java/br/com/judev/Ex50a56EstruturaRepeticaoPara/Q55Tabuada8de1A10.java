e62367aab4263831cda20018c6b20e88dd8c54e9
Junior-Stranner <JuniorStranner@gmail.com>
2024-08-13 10:58:40 -0300

    Exercise 55 until 59 done

---
+++ JudevTrainAcademy/algoritmos1to100/src/main/java/br/com/judev/Ex50a56EstruturaRepeticaoPara/Q55Tabuada8de1A10.java
@@ -0,0 +1,12 @@
+package br.com.judev.Ex50a56EstruturaRepeticaoPara;
+
+public class Q55Tabuada8de1A10 {
+    public static void main(String[] args) {
+        
+        for(int x =1; x <11 ; x++){
+        //    System.out.println( 8 * x);
+        int resultado = 8 * x;
+        System.out.println(8 +" x "+x+" = "+resultado);
+        }
+    }
+}
