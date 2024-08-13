888e5a7472f060bbbfabd47a36dab92c5810ec5e
Junior-Stranner <JuniorStranner@gmail.com>
2024-08-13 11:50:54 -0300

    Ex 60 until 63

---
+++ JudevTrainAcademy/algoritmos1to100/src/main/java/br/com/judev/EX57a69ContadoresEacumuladores/Q63Ler10numsCalculaAsome.java
@@ -0,0 +1,20 @@
+package br.com.judev.EX57a69ContadoresEacumuladores;
+
+import java.util.Scanner;
+
+public class Q63Ler10numsCalculaAsome {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+                int soma = 0;
+                int numero;
+        
+                for (int i = 1; i <= 10; i++) {
+                    System.out.print("Digite o número " + i + ": ");
+                    numero = Integer.parseInt(in.nextLine());
+                    soma += numero;
+                }
+                System.out.println("A soma total dos números é: " + soma);
+        }
+        
+}
