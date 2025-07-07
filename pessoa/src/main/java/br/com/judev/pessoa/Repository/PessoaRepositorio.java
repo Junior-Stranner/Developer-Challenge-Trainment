54d4d7bc35d3efd13a59f00880bf875554741397
Junior-Stranner <JuniorStranner@gmail.com>
2025-07-07 09:43:45 -0300

    change some methods and rules

--- pessoa/src/main/java/br/com/judev/pessoa/Repository/PessoaRepositorio.java
+++ pessoa/src/main/java/br/com/judev/pessoa/Repository/PessoaRepositorio.java
@@ -6,6 +6,20 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface PessoaRepositorio extends JpaRepository<Pessoa, Long> {
 
+    Iterable<Pessoa> findByCidade(String cidade);
+
+    Iterable<Pessoa> findByCidadeOrCidade(String cidade1, String cidade2);
+
+    Iterable<Pessoa> findByIdadeGreaterThanEqual(Integer idade);
+
+    Iterable<Pessoa> findByIdadeBetween(Integer idade1, Integer idade2);
+
+    Iterable<Pessoa> findByNomeLikeIgnoreCase(String nome);
+
+    Iterable<Pessoa> findAllByOrderByNomeAsc();
+
+    Long countByCidade(String cidade);
+
     @Query("SELECT SUM(p.idade) FROM PessoaModelo p")
     Long calcularSomaIdades();
 }
